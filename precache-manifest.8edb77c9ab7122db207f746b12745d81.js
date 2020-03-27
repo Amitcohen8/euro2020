0aa44a0bcc68aa1e22899d6ad2de651087aa35cb
Amit Cohen <amicohen@corp.devalore.com>
2020-03-27 09:55:43 +0300

    Updates

---
+++ precache-manifest.8edb77c9ab7122db207f746b12745d81.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5bc2444321831653caeddf41b9d898",
+    "url": "/euro2020/index.html"
+  },
+  {
+    "revision": "fb0c9700969afc08d224",
+    "url": "/euro2020/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8441f9e5097221dd3d23",
+    "url": "/euro2020/static/css/main.fef6cb8e.chunk.css"
+  },
+  {
+    "revision": "fb0c9700969afc08d224",
+    "url": "/euro2020/static/js/2.dfc8d92e.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/euro2020/static/js/2.dfc8d92e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8441f9e5097221dd3d23",
+    "url": "/euro2020/static/js/main.e1168ef7.chunk.js"
+  },
+  {
+    "revision": "354eeeb9d96140294f41",
+    "url": "/euro2020/static/js/runtime-main.04c2d563.js"
+  },
+  {
+    "revision": "74450a3d320db71688562c18ead0d738",
+    "url": "/euro2020/static/media/euro2020_1.74450a3d.jpg"
+  },
+  {
+    "revision": "3960691400d1ea9361e676d4cbfb27d6",
+    "url": "/euro2020/static/media/logo.39606914.png"
+  },
+  {
+    "revision": "b247017f34cd736e2f86e0a0742929d8",
+    "url": "/euro2020/static/media/ronaldo1.b247017f.jpg"
+  }
+]);
